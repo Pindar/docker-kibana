358edf552f3b831b38aeeab40d03944f162a6ad6
Simon Dittlmann <simon@itnotes.de>
2015-03-01 17:29:10 +0100

    fix: remove syntax error

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 
 sed -i "s/elasticsearch: \"http:\/\/localhost:9200\"/elasticsearch: \"$(echo $ELASTICSEARCH_ENDPOINT | sed -e 's/[]\/$*.^|[]/\\&/g')\"/" /kibana-4.0.0-linux-x64/config/kibana.yml
 
-if ! [[ $USER == "" -a $PASS == "" ]]; then
+if ! [[ $USER == "" && $PASS == "" ]]; then
 	echo "elasticsearch_username: $USER" >> /kibana-4.0.0-linux-x64/config/kibana.yml
 	echo "elasticsearch_password: $PASS" >> /kibana-4.0.0-linux-x64/config/kibana.yml
 fi
