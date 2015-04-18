aa8070566d866a422610076a1b0bba907fb54b73
Simon Dittlmann <simon@itnotes.de>
2015-04-18 09:52:56 +0200

    Revert "update base image to alpine"

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
 
-sed -i "s/elasticsearch_url: \"http:\/\/localhost:9200\"/elasticsearch_url: \"$(echo $ELASTICSEARCH_ENDPOINT | sed -e 's/[]\/$*.^|[]/\\&/g')\"/" /kibana-${KIBANA_VERSION}-linux-x64/config/kibana.yml
+sed -i "s/elasticsearch_url: \"http:\/\/localhost:9200\"/elasticsearch_url: \"$(echo $ELASTICSEARCH_ENDPOINT | sed -e 's/[]\/$*.^|[]/\\&/g')\"/" /kibana-4.0.0-linux-x64/config/kibana.yml
 
 if ! [[ $USER == "" && $PASS == "" ]]; then
-	echo "kibana_elasticsearch_username: $USER" >> /kibana-${KIBANA_VERSION}-linux-x64/config/kibana.yml
-	echo "kibana_elasticsearch_password: $PASS" >> /kibana-${KIBANA_VERSION}-linux-x64/config/kibana.yml
+	echo "kibana_elasticsearch_username: $USER" >> /kibana-4.0.0-linux-x64/config/kibana.yml
+	echo "kibana_elasticsearch_password: $PASS" >> /kibana-4.0.0-linux-x64/config/kibana.yml
 fi
 
 
-exec /kibana-${KIBANA_VERSION}-linux-x64/bin/kibana
+exec /kibana-4.0.0-linux-x64/bin/kibana
